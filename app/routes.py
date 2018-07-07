744dba3c58b4155e92d7f4857d95c4d3caed8ba1
Naveen M V <naveenmv7@gmail.com>
2018-07-07 23:01:57 +0530

    Implement refactor

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify
 from app import app, db
 from app.models import Flight, Itinerary, Journey
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -26,15 +27,53 @@ def display():
 
     data = res.json()
 
-    # Check for duplicates?
+    # Add condition to update database
+    if False:
+        add_to_database(origin, destination, data)
 
+    
+
+    return jsonify(refactor_data(origin, destination, date, data))
+    # return render_template("display.html", journey=j)
+
+
+
+def add_to_database(origin, destination, date, data):
     j = Journey(origin=origin, destination=destination, date=str(date))
     db.session.add(j)
     db.session.commit()
-
     j.add_itinerary(data['results'])
 
 
+def refactor_data(origin, destination, date, data):
+    new_data = {'origin': origin, 'destination': destination,
+                'date': date}
+
+    results = data['results']
+   
+    new_itineraries = []
+
+    for result in results:
+        it = {'duration': result['itineraries'][0]['outbound']['duration'],
+            'price_per_adult': result['fare']['price_per_adult']['total_fare'],
+            'tax': result['fare']['price_per_adult']['tax']}
+        
+        flights = result['itineraries'][0]['outbound']['flights']
+        new_flights = []
+        for flight in flights:
+            f = {'flight_number': flight['flight_number'],
+                'airline': flight['operating_airline'],
+                'origin': flight['origin']['airport'],
+                'destination': flight['destination']['airport'],
+                'departure_time': flight['departs_at'],
+                'arrival_time': flight['arrives_at']}
+            new_flights.append(f)
+        it['flights'] =  new_flights
+        new_itineraries.append(it)
+
+    new_data['itineraries'] = new_itineraries
+
+    return new_data
 
 
-    return jsonify(data)
+    
