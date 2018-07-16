6c6542a6bd680d42bfd0007e1121b064c647a0d8
Naveen M V <naveenmv7@gmail.com>
2018-07-16 21:17:05 +0530

    Save got a makeover

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, json
 from app import app, db
 from app.models import Flight, Itinerary, Journey
 
@@ -8,50 +8,50 @@ from app.helper import add_to_database, refactor_data
 
 @app.route("/")
 def index():
-    iata = [
-        {'name': 'New Delhi, India',
-         'code': 'DEL'},
-        {'name': 'Mumbai, India',
-         'code': 'BOM'},
-         {'name': 'Bangalore, India',
-         'code': 'BLR'},
-         {'name': 'Goa, India',
-         'code': 'GOI'},
-         {'name': 'Chennai, India',
-         'code': 'MAA'},
-         {'name': 'Kolkata, India',
-         'code': 'CCU'},
-         {'name': 'HYD, India',
-         'code': 'HYD'},
-         {'name': 'Pune, India',
-         'code': 'PNQ'},
-         {'name': 'Ahmedabad, India',
-         'code': 'AMD'},
-         {'name': 'Cochin, India',
-         'code': 'COK'},
-         {'name': 'Jaipur, India',
-         'code': 'JAI'},
-         {'name': 'Dubai, UAE',
-         'code': 'DXB'},
-         {'name': 'Singapore, Singapore',
-         'code': 'SIN'},
-         {'name': 'Bangkok, Thailand',
-         'code': 'DEL'},
-         {'name': 'New York, US - All Airports',
-         'code': 'NYC'},
-         {'name': 'Kuala Lumpur, Malaysia',
-         'code': 'KUL'},
-         {'name': 'London, UK - All Airports',
-         'code': 'LON'},
-         {'name': 'Hong Kong, China',
-         'code': 'HKG'},
-         {'name': 'Doha, Qatar',
-         'code': 'DOH'},
-         {'name': 'Colombo, Sri Lanka',
-         'code': 'CMB'}
-    ]
+    # iata = [
+    #     {'name': 'New Delhi, India',
+    #      'code': 'DEL'},
+    #     {'name': 'Mumbai, India',
+    #      'code': 'BOM'},
+    #      {'name': 'Bangalore, India',
+    #      'code': 'BLR'},
+    #      {'name': 'Goa, India',
+    #      'code': 'GOI'},
+    #      {'name': 'Chennai, India',
+    #      'code': 'MAA'},
+    #      {'name': 'Kolkata, India',
+    #      'code': 'CCU'},
+    #      {'name': 'HYD, India',
+    #      'code': 'HYD'},
+    #      {'name': 'Pune, India',
+    #      'code': 'PNQ'},
+    #      {'name': 'Ahmedabad, India',
+    #      'code': 'AMD'},
+    #      {'name': 'Cochin, India',
+    #      'code': 'COK'},
+    #      {'name': 'Jaipur, India',
+    #      'code': 'JAI'},
+    #      {'name': 'Dubai, UAE',
+    #      'code': 'DXB'},
+    #      {'name': 'Singapore, Singapore',
+    #      'code': 'SIN'},
+    #      {'name': 'Bangkok, Thailand',
+    #      'code': 'DEL'},
+    #      {'name': 'New York, US - All Airports',
+    #      'code': 'NYC'},
+    #      {'name': 'Kuala Lumpur, Malaysia',
+    #      'code': 'KUL'},
+    #      {'name': 'London, UK - All Airports',
+    #      'code': 'LON'},
+    #      {'name': 'Hong Kong, China',
+    #      'code': 'HKG'},
+    #      {'name': 'Doha, Qatar',
+    #      'code': 'DOH'},
+    #      {'name': 'Colombo, Sri Lanka',
+    #      'code': 'CMB'}
+    # ]
 
-    return render_template("index.html", iata=iata)
+    return render_template("index.html")
 
 @app.route("/saved", methods=['GET'])
 def saved():
@@ -86,3 +86,25 @@ def display():
 @app.route("/export/<int:journey_id>", methods=['GET'])
 def export(journey_id):
     return render_template("export.html", data=Journey.query.get(journey_id).serialize)
+
+
+
+@app.route("/delete", methods=['POST'])
+def delete():
+    try:
+        id = request.json['jid']
+        j = Journey.query.get(id)
+        it = j.itineraries
+        for i in it:
+            f = i.flights
+            for j in f:
+                db.session.delete(j)
+            db.session.delete(i)
+        db.session.delete(j)
+        db.session.commit()
+
+        return "success", 200
+    except:
+        return "error", 404
+    
+
